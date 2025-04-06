37457bbdd1cdb0da43b558fca2da7deaaac79243
ShreyashM17 <shreyash.m@fyle.in>
2025-04-06 16:02:16 +0530

    ip

--- sustagram_backend/settings.py
+++ sustagram_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-719l3(ema$=h8x8p6-0bqp(8y^bs5rkfy&ak3b3jxb*rnddilp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
